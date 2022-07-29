557fb99488e9b2bbc4e71ddb6d312265ac9f82e0
Manoj Sharma <manojsharma.mha@gmail.com>
2022-07-29 12:21:09 +0530

    again cascade in votes

--- app/models.py
+++ app/models.py
@@ -36,6 +36,6 @@ class User(Base):
 class Votes(Base):
     __tablename__ = "votes"
 
-    user_id = Column(Integer, ForeignKey("users.id", ondelete=CASCADE), primary_key=True )
-    post_id = Column(Integer, ForeignKey("posts.id", ondelete=CASCADE), primary_key=True )
+    user_id = Column(Integer, ForeignKey("users.id", ondelete='CASCADE'), primary_key=True )
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete='CASCADE'), primary_key=True )
     phone_number = Column(Integer)
